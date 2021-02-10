c8fcc471ac18a2f14ce68df4464e6a3d8bf585bc
GomezOrtiz <dgomez@nextdigital.es>
2021-02-10 23:45:17 +0100

    Cors config, BaseControllerTest and better application config

---
+++ src/main/java/es/nextdigital/springprojectboilerplate/config/cors/CorsConfig.java
@@ -0,0 +1,28 @@
+package es.nextdigital.springprojectboilerplate.config.cors;
+
+import es.nextdigital.springprojectboilerplate.config.AppProfiles;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    @Profile(AppProfiles.DEV)
+    public FilterRegistrationBean<CorsFilter> devCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+}
