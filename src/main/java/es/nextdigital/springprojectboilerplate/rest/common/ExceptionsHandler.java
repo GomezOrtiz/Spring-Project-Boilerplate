c3bec1f996f91a94d3b6766321c99034a40d9394
GomezOrtiz <dgomez@nextdigital.es>
2021-02-10 23:55:49 +0100

    Log stack trace in ExceptionsHandler for InternalServerError

--- src/main/java/es/nextdigital/springprojectboilerplate/rest/common/ExceptionsHandler.java
+++ src/main/java/es/nextdigital/springprojectboilerplate/rest/common/ExceptionsHandler.java
@@ -2,6 +2,7 @@ package es.nextdigital.springprojectboilerplate.rest.common;
 
 import es.nextdigital.springprojectboilerplate.application.common.exception.*;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -170,7 +171,7 @@ public class ExceptionsHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({ Exception.class })
     public ResponseEntity<Object> handleAll(Exception ex, WebRequest request) {
-        log.error("Internal Server Error: " + ex);
+        log.error("Internal Server Error: " + ExceptionUtils.getStackTrace(ex));
         ApiError apiError = new ApiError(
                 HttpStatus.INTERNAL_SERVER_ERROR, ex.getLocalizedMessage(), "Error occurred");
         return new ResponseEntity<>(
